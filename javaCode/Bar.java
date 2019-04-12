28ba2710cf06e4cde456cc740a43e1a0fcebce5c
thejfromc <35538225+thejfromc@users.noreply.github.com>
2019-04-12 13:24:34 -0600

    Update Bar.java

--- javaCode/Bar.java
+++ javaCode/Bar.java
@@ -7,18 +7,18 @@ public class Bar extends Foo{
 	public void Doo() {
 		for(int i = 0;i < super.bins.size();i = i + 3) {
 			
-			((Bin) bins.get(i)).Pop();
+			((bin) bins.get(i)).pop();
 			
 		}
 		for(int i = 1;i < super.bins.size();i = i + 3) {
 			
-			((Bin) bins.get(i)).Pop();
+			((bin) bins.get(i)).pop();
 			
 		}
 		for(int i = 2;i < super.bins.size();i = i + 3) {
 			
-			((Bin) bins.get(i)).Pop();
+			((bin) bins.get(i)).pop();
 			
 		}	
-	}	
+	}
 }
