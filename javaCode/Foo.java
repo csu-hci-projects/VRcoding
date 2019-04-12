2f3f41a1b4c53881177da9a4c1880f31d8ddb30e
thejfromc <35538225+thejfromc@users.noreply.github.com>
2019-04-12 13:23:25 -0600

    Update Foo.java

--- javaCode/Foo.java
+++ javaCode/Foo.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Foo {
 
-	public ArrayList bins = new ArrayList<Bin>();
+	public ArrayList bins = new ArrayList<bin>();
 	
 	
 	public void Doo () {
 		
 		for(int i = 0; i < bins.size();i++ ){
 			
-			((Bin) bins.get(i)).Pop();
+			((bin) bins.get(i)).pop();
 				
 		}
 	}
